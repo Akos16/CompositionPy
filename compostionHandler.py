a0e6a0ec3a0d46f80ebbbf3d7e0b39c6b7ec3f12
Akos16 <denkingerakos2003@gmail.com>
2026-05-12 21:12:47 +0200

    Test data manipulation

--- compostionHandler.py
+++ compostionHandler.py
@@ -4,7 +4,12 @@
 #composition[0]["18"][260] [0. txt]["lgE"][index, txt 1. sora a 0. elem]
 class compositionHandler():
     def weightedComposition(self, composition):
-        comp = []
+        comp0 = []
+        comp1 = []
+        comp2 = []
+        comp3 = []
+        tempComp0 = []
+        tempParamComp = composition[0]["18"][180]
         '''
         for i in range(3):
             comp.append(composition[0]["18"][i])
@@ -18,6 +23,14 @@ class compositionHandler():
                 comp.append(composition[i]["18"][j] * nums[i])
         '''
         for i in range(len(composition[0]["18"])):
-            comp.append(composition[0]["18"][i] * 0.75)
-        comZSum = sum(comp)
-        return comZSum
+            comp0.append(composition[0]["18"][i] * 0.75)
+        tempComp0 = composition[0]["18"] * 0.75
+        for j in range(len(composition[1]["18"])):
+            comp1.append(composition[1]["18"][j])
+        for k in range(len(composition[2]["18"])):
+            comp2.append(composition[2]["18"][k])
+        for z in range(len(composition[3]["18"])):
+            comp3.append(composition[3]["18"][z])
+        comLSum = len(comp0) + len(comp1) + len(comp2) + len(comp3)
+        comZSum = sum(comp0 + comp1 + comp2 + comp3) / comLSum 
+        return comZSum, comp0[180], tempComp0[180], tempParamComp
--- main.py
+++ main.py
@@ -5,4 +5,6 @@ dataClass = dataHandler()
 data = dataClass.getData()
 compHandler = compositionHandler()
 wComp = compHandler.weightedComposition(data)
-print(wComp)
+print(wComp[0])
+
+print(f"Comp0: {wComp[1]}, TempComp0: {wComp[2]}, ParamComp0: {wComp[3]}")
