bf0499182048587705c31fecf2e6c9cae685d47a
Akos16 <denkingerakos2003@gmail.com>
2026-07-16 23:00:13 +0200

    Plot lgE 18 xMax

--- main.py
+++ main.py
@@ -6,3 +6,20 @@ import pandas as pd
 
 monteCarloData = monteCarloDataHandler().getMonteCarloData()
 xMaxData = xMaxDataHandler().getXmaxData()
+
+
+
+i = 0
+tempArray = []
+while i < 50:
+    tempArray.append(monteCarloData[4][i])
+    i += 1
+
+
+
+plt.plot(tempArray, xMaxData[0][1], color="r")
+
+plt.xlabel("Xmax")
+plt.title("Xmax test")
+plt.legend()
+plt.show()
