31607be0956a31113eb79d55444a4e25b0520f31
Akos16 <denkingerakos2003@gmail.com>
2026-07-25 23:14:53 +0200

    Plot lgE 18 Xmax, lgE 18 MC

--- compostionHandler.py
+++ compostionHandler.py
@@ -1,8 +1,7 @@
 #comp = norm*(a*arr1 + b*arr2+ c*arr3 + d*arr4)
 #0 <= a, b, c, d <= 1  ugy hogy a+b+c+d=1
-#composition[0]["18"][260] [0. txt]["lgE"][index, txt 1. sora a 0. elem]
 class compositionHandler():
-    def weightedComposition(self, comp0, comp1, comp2, comp3):
+    def model(self, x, comp0, comp1, comp2, comp3):
         comp = comp0 * 1 + comp1 * 0 + comp2 * 0 + comp3 * 0
         return comp
     
--- main.py
+++ main.py
@@ -7,19 +7,37 @@ import pandas as pd
 monteCarloData = monteCarloDataHandler().getMonteCarloData()
 xMaxData = xMaxDataHandler().getXmaxData()
 
-
-
+#first 50 element, xmax length = 50
 i = 0
-tempArray = []
+xAxis = []
 while i < 50:
-    tempArray.append(monteCarloData[4][i])
+    xAxis.append(monteCarloData[4][i])
     i += 1
 
+arr1 = monteCarloData[0][1]
+arr2 = monteCarloData[1][1]
+arr3 = monteCarloData[2][1]
+arr4 = monteCarloData[3][1]
+#parameters
+a = 0.398609
+b = 0.032087
+c = 0.414095
+d = 0.15521
+#function
+model = a*arr1['Frac'] + b*arr2['Frac'] + c*arr3['Frac'] + d*arr4['Frac']
 
+#first 50 element (model), xmax length = 50
+y = 0
+counter = []
+while y < 50:
+    counter.append(model[y])
+    y += 1
 
-plt.plot(tempArray, xMaxData[0][1], color="r")
-
+#plot xMax, lgE 18
+plt.plot(xAxis, xMaxData[0][1], '.', color="r", label='Auger Xmax data')
+plt.plot(xAxis, counter, 'o', markersize=3, color='b', label='MC composition')
 plt.xlabel("Xmax")
-plt.title("Xmax test")
+plt.title("MC composition on Xmax data")
 plt.legend()
 plt.show()
+
