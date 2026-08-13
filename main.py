8a9b77f26abd076d1d3bef4af690855f828839cf
Akos16 <denkingerakos2003@gmail.com>
2026-08-13 13:25:09 +0200

    Change minimize() to optimize.minimize(), calculate chi with for loop

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from monteCarloDataHandler import monteCarloDataHandler
 from xMaxDataHandler import xMaxDataHandler
 from compostionHandler import compositionHandler
 from lmfit import create_params, fit_report, minimize
+from scipy.optimize import minimize                                             ## For the minimalization
+import scipy.optimize as optimization                                           ## For the optimization
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-
+from scipy import optimize
 
 monteCarloData = monteCarloDataHandler().getMonteCarloData()
 xMaxData = xMaxDataHandler().getXmaxData()
@@ -27,7 +29,7 @@ print(mean, var, skew, kurt)
 #first 50 element, xmax length = 50
 i = 0
 xAxis = []
-while i < 50:
+while i < 48:
     xAxis.append(monteCarloData[4][i])
     i += 1
 
@@ -36,49 +38,76 @@ arr2 = monteCarloData[1][1]
 arr3 = monteCarloData[2][1]
 arr4 = monteCarloData[3][1]
 
-data = np.array(xMaxData[0][1][:50], dtype=float)
-params = create_params(a=0.398609,b=0.032087,c=0.414095,d=0.15521)
+data = np.array(xMaxData[0][1][:48], dtype=float)
+params = create_params(a=0.68,b=0.11,c=0.17,d=0.04)
 
-def residual(pars, data=None):
-    """Model a decaying sine wave and subtract data."""
-    vals = pars.valuesdict()
-    a = vals['a']
-    b = vals['b']
-    c = vals['c']
-    d = vals['d']
 
-    model = (a*arr1["Frac"][:50] + b*arr2["Frac"][:50] + c*arr3["Frac"][:50] + d*arr4["Frac"][:50])   
-    if data is None:
-        return model
-    return model - data
 
-out = minimize(residual, params, kws={"data": data})
-testString = (
-    f"\nData points: {out.ndata}"
-    f"\nchi-square: {out.chisqr}"
-    f"\nredchi: {out.redchi}\n"
+def residual(pars):
+    """Calculate chi-square between model and data."""
+
+    a = pars[0]
+    b = pars[1]
+    c = pars[2]
+    d = pars[3]
+
+    model = (
+        a * arr1["Frac"][:48]
+        + b * arr2["Frac"][:48]
+        + c * arr3["Frac"][:48]
+        + d * arr4["Frac"][:48]
+    )
+
+    chi = 0.0
+
+    for i in range(len(model)):
+        if data[i] != 0:
+            chi += ((data[i] - model[i])/np.sqrt(data[i])) ** 2 
+
+    return chi
+
+#mc, xmax skew és kurt különbsége minél kisebb legyen
+# Initial parameter values
+params = np.array([
+    0.4,  # a
+    0.03,  # b
+    0.41,  # c
+    0.15   # d
+])
+
+# Optimization
+out = optimize.minimize(
+    residual,
+    params,
+    method="SLSQP"
 )
 
-with open("outputDatas/out.txt", "w") as f:
-    f.write(fit_report(out))
-    f.write(testString)
+print("Success:", out.success)
+print("Message:", out.message)
+
+print("a =", out.x[0])
+print("b =", out.x[1])
+print("c =", out.x[2])
+print("d =", out.x[3])
+
+print("chi2 =", out.fun)
 
 
-a_fit = out.params["a"].value
-b_fit = out.params["b"].value
-c_fit = out.params["c"].value
-d_fit = out.params["d"].value
+a_fit = out.x[0]
+b_fit = out.x[1]
+c_fit = out.x[2]
+d_fit = out.x[3]
 
 model_fit = (
-    a_fit*arr1["Frac"][:50]
-    + b_fit*arr2["Frac"][:50]
-    + c_fit*arr3["Frac"][:50]
-    + d_fit*arr4["Frac"][:50]
+    a_fit*arr1["Frac"][:48]
+    + b_fit*arr2["Frac"][:48]
+    + c_fit*arr3["Frac"][:48]
+    + d_fit*arr4["Frac"][:48]
 )
 
 #plot xMax, lgE 18
-plt.plot(xAxis, xMaxData[0][1], '-', color="r", label='Auger Xmax data')
-plt.plot(xAxis, model_fit, '-', markersize=3, color='b', label='MC composition')
+plt.plot(xAxis, xMaxData[0][1][:48], 'o', markersize=2, color="r", label='Auger Xmax data')
+plt.plot(xAxis, model_fit, 'o', markersize=2, color='b', label='MC composition')
 plt.xlabel("Xmax")
 plt.title("MC composition on Xmax data")
 plt.legend()
