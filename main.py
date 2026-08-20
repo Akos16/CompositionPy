e5aed6f7d36a922a368aa74e032751f3c7962612
Akos16 <denkingerakos2003@gmail.com>
2026-08-20 13:29:40 +0200

    Update ReadAndRebin function, add constraints to minimize

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ monteCarloData = monteCarloDataHandler().getMonteCarloData()
 xMaxHandler = xMaxDataHandler()
 xMaxData = xMaxHandler.getXmaxData()
 
+
 #get momentums with erros from xMax data
 meandata, meandata_err, vardata, vardata_err, skewdata, skewdata_err, kurtdata, kurtdata_err = xMaxHandler.moments_with_errors(xMaxData[0][0][:48], xMaxData[0][1][:48], xMaxData[0][2][:48])
 
@@ -27,54 +28,65 @@ mcrows = [] #for 4 mc sim to calculate skewness
 for i in range(len(arr1)):
     mcrows.append(arr1["Frac"][i] + arr2["Frac"][i] + arr3["Frac"][i] + arr4["Frac"][i])
 
-mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], mcrows[:48])
+#mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], mcrows[:48])
 #print(f"xmax", mean, var, skew, kurt)
 #print(f"mc", mcmean, mcmu2, mcskew, mc_excesskurt)
 
 data = np.array(xMaxData[0][1][:48], dtype=float)
-params = create_params(a=0.68,b=0.11,c=0.17,d=0.04)
-print(data)
-def residual(pars):
-    """Calculate chi-square between model and data."""
-
+params = create_params(a=0.10,b=0.25,c=0.25,d=0.40)
+ 
+def model(pars):
     a = pars[0]
     b = pars[1]
     c = pars[2]
     d = pars[3]
+    return a * arr1["Frac"] + b * arr2["Frac"] + c * arr3["Frac"] + d * arr4["Frac"]
+ 
+def residual(pars):
 
-    model = (a * arr1["Frac"] + b * arr2["Frac"] + c * arr3["Frac"] + d * arr4["Frac"])
-
+    this_model = model(pars)
+    mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], this_model)
     chi = 0.0
-
-    for i in range(len(model)):
+    for i in range(len(this_model)):
         if data[i] != 0:
-            chi += ((data[i] - model[i])/np.sqrt(data[i])) ** 2 
-    chi += ((skewdata - mcskew) / skewdata_err) ** 2
-    chi += ((kurtdata - mc_excesskurt) / kurtdata_err) ** 2
-    #chi += (skewness_data - skewness_MC) / skewness_err
-	#chi += (kurtosis_data - kurtosis_MC) / kurtosis_err
-
+            chi += ((data[i] - this_model[i])/np.sqrt(data[i])) ** 2
     return chi
+
+def mean(pars):
+    this_model = model(pars)
+    mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], this_model)
+    return ((meandata - mcmean) / meandata_err) ** 2
+def mu(pars):
+    this_model = model(pars)
+    mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], this_model)
+    return ((vardata - mcmu2) / vardata_err) ** 2
+
+def skew(pars):
+    this_model = model(pars)
+    mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], this_model)
+    return ((skewdata - mcskew) / skewdata_err) ** 2
+def kurt(pars):
+    this_model = model(pars)
+    mcmean, mcmu2, mcskew, mc_excesskurt = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], this_model)
+    return ((kurtdata - mc_excesskurt) / kurtdata_err) ** 2
 
 #mc, xmax skew és kurt különbsége minél kisebb legyen
 # Initial parameter values, a, b, c, d
-params = np.array([0.4, 0.03, 0.41, 0.15])
 
 # Optimization
-
 bounds = [ #https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.Bounds.html#scipy.optimize.Bounds
     (0, 1),  # a
     (0, 1),  # b
     (0, 1),  # c
     (0, 1)   # d
 ]
-
 #?
-constraints = {
-    "type": "eq",
-    "fun": lambda x: np.sum(x) - 1
-}
-
+constr_mean = {"type":"eq", "fun": mean}
+constr_mu = {"type":"eq", "fun": mu}
+constr_skew = { "type": "eq", "fun": skew }
+constr_kurt = { "type": "eq", "fun": kurt }
+constr_unit = {"type":"eq", "fun": lambda x: np.sum(x) - 1}
+constraints = [constr_mean, constr_mu, constr_unit]
 out = optimize.minimize(
     residual,
     params,
@@ -100,6 +112,9 @@ c_fit = out.x[2]
 d_fit = out.x[3]
 
 model_fit = (a_fit*arr1["Frac"][:48] + b_fit*arr2["Frac"][:48] + c_fit*arr3["Frac"][:48] + d_fit*arr4["Frac"][:48])
+mcmeantest, mcmu2test, mcskewtest, mc_excesskurttest = xMaxHandler.moments_from_prob(xMaxData[0][0][:48], model_fit)
+print(f"Model skew: ", mcskewtest, "Auger skew: ", skewdata, "Auger skewerr: ", skewdata_err)
+print(f"Model kurt: ", mc_excesskurttest, "Auger kurt: ", kurtdata, "Auger kurt: ", kurtdata_err)
 #ax.errorbar(x, y, yerr=yerr, fmt='o', markersize=1, capsize=1, elinewidth=1, color='black', zorder=3, label='Mért adat')
 #plot xMax, lgE 18
 plt.errorbar(monteCarloData[4][:48], xMaxData[0][1][:48], yerr=xMaxData[0][2][:48], fmt='o', markersize=1, capsize=1, elinewidth=1, color='black', zorder=3, label='Auger Xmax data')
--- monteCarloDataHandler.py
+++ monteCarloDataHandler.py
@@ -1,64 +1,140 @@
 import os
 import pandas as pd
 import numpy as np
+
+
 class monteCarloDataHandler:
+
     def readAndRebin(self, filename, start=504, step=12):
         df = pd.read_csv(filename, sep="\t", decimal=",")
 
         new_lines = []
 
-        i = 0
-        while i < len(df):
-            if df.iloc[i, 0] < start:
-                i += 1
+        # Minden bin-pár külön van kezelve
+        for col_idx in range(0, len(df.columns), 2):
+
+            bin_col = df.columns[col_idx]
+            value_col = df.columns[col_idx + 1]
+
+            # Csak a start-tól kezdődő adatok
+            data = df[df[bin_col] >= start].copy()
+
+            if data.empty:
                 continue
 
-            line = {"bin": df.iloc[i, 0]}
+            # Az eredményhez tartozó bin értékek
+            bins = range(
+                start,
+                int(data[bin_col].max()) + 1,
+                step
+            )
 
-            for col in df.columns[1:]:
-                osszeg = 0
-                for j in range(step):
-                    if i + j < len(df):
-                        osszeg += df.iloc[i + j][col]
-                line[col] = osszeg
+            # Első oszlopnál létrehozzuk a sorokat
+            if col_idx == 0:
+                for new_bin in bins:
 
-            new_lines.append(line)
-            i += step
+                    mask = (
+                        (data[bin_col] >= new_bin) &
+                        (data[bin_col] < new_bin + step)
+                    )
 
-        return pd.DataFrame(new_lines)         
+                    osszeg = data.loc[mask, value_col].sum()
+
+                    new_lines.append([new_bin, osszeg])
+
+            else:
+                # A további energiaoszlopok értékeit
+                # hozzáadjuk a már meglévő sorokhoz
+                for row, new_bin in zip(new_lines, bins):
+
+                    mask = (
+                        (data[bin_col] >= new_bin) &
+                        (data[bin_col] < new_bin + step)
+                    )
+
+                    osszeg = data.loc[mask, value_col].sum()
+
+                    row.append(new_bin)
+                    row.append(osszeg)
+
+        return new_lines
 
     def getMonteCarloData(self):
+
         folder = "./MonteCarloSimulations"
         files = [f for f in os.listdir(folder) if f.endswith(".txt")]
         n_files = len(files) - 1
-        
+
         lgEs = ["17,5", "18", "18,5", "19"]
-        #Xmax-ok
-        componentsX = []
-        filenameX = f"./MonteCarloSimulations/component0.txt"
-        df = self.readAndRebin(filenameX)
-        
-        
-        componentsX = df["bin"]
+
+        # Xmax-ok
+        filenameX = "./MonteCarloSimulations/component0.txt"
+
+        data = self.readAndRebin(filenameX)
+
+        df = pd.DataFrame(data, columns=[
+            "bin", "17,5",
+            "bin", "18",
+            "bin", "18,5",
+            "bin", "19"
+        ])
+
+        componentsX = df.iloc[:, 0]
+
         componentsY0 = []
         componentsY1 = []
         componentsY2 = []
         componentsY3 = []
+
         for i in range(n_files):
+
             filename1 = f"./MonteCarloSimulations/component{i}.txt"
-            df = self.readAndRebin(filename1)
-            match i: 
-                case 0: 
+
+            data = self.readAndRebin(filename1)
+
+            df = pd.DataFrame(data, columns=[
+                "bin", "17,5",
+                "bin", "18",
+                "bin", "18,5",
+                "bin", "19"
+            ])
+
+            match i:
+
+                case 0:
                     for y in lgEs:
-                        componentsY0.append(pd.DataFrame({'Xmax': componentsX.values, 'Frac': df[y].values / df[y].values.sum()}))
-                case 1: 
+                        componentsY0.append(
+                            pd.DataFrame({
+                                'Xmax': componentsX.values,
+                                'Frac': df[y].values / df[y].values.sum()
+                            })
+                        )
+
+                case 1:
                     for y in lgEs:
-                        componentsY1.append(pd.DataFrame({'Xmax': componentsX.values, 'Frac': df[y].values / df[y].values.sum()}))
-                case 2: 
+                        componentsY1.append(
+                            pd.DataFrame({
+                                'Xmax': componentsX.values,
+                                'Frac': df[y].values / df[y].values.sum()
+                            })
+                        )
+
+                case 2:
                     for y in lgEs:
-                        componentsY2.append(pd.DataFrame({'Xmax': componentsX.values, 'Frac': df[y].values / df[y].values.sum()}))
-                case 3: 
+                        componentsY2.append(
+                            pd.DataFrame({
+                                'Xmax': componentsX.values,
+                                'Frac': df[y].values / df[y].values.sum()
+                            })
+                        )
+
+                case 3:
                     for y in lgEs:
-                        componentsY3.append(pd.DataFrame({'Xmax': componentsX.values, 'Frac': df[y].values / df[y].values.sum()}))
-        
+                        componentsY3.append(
+                            pd.DataFrame({
+                                'Xmax': componentsX.values,
+                                'Frac': df[y].values / df[y].values.sum()
+                            })
+                        )
+
         return componentsY0, componentsY1, componentsY2, componentsY3, componentsX
